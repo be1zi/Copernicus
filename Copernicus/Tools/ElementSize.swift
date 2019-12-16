659a8a2e6c09e925b263ff858a964a5ba86048c1
Konrad Bełzowski <konrad.belzowski@gmail.com>
2019-12-16 13:58:29 +0100

    language view part 1

---
+++ Copernicus/Tools/ElementSize.swift
@@ -0,0 +1,17 @@
+//
+//  ElementSize.swift
+//  Copernicus
+//
+//  Created by Konrad Belzowski on 16/12/2019.
+//  Copyright © 2019 Konrad Bełzowski. All rights reserved.
+//
+
+import UIKit
+
+enum ElementSize {
+    
+    enum Button: CGFloat {
+        case width = 220.0
+        case height = 44.0
+    }
+}
