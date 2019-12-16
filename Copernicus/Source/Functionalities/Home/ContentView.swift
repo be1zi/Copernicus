f73b1001a72ed2ce65702c41c9b0a178edb33b84
Konrad Bełzowski <konrad.belzowski@gmail.com>
2019-12-16 11:51:07 +0100

    add localization

--- Copernicus/Source/Functionalities/Home/ContentView.swift
+++ Copernicus/Source/Functionalities/Home/ContentView.swift
@@ -9,8 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let text = "test.test".localized()
+    
     var body: some View {
-        Text("Hello, World!")
+        Text(text)
     }
 }
 
