a935633dbf7236012e841c78d1cac469df5ae327
Konrad Bełzowski <konrad.belzowski@gmail.com>
2019-12-16 08:08:01 +0100

    add base view controller

---
+++ Copernicus/UI/BaseViewController/BaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.swift
+//  Copernicus
+//
+//  Created by Konrad Belzowski on 16/12/2019.
+//  Copyright © 2019 Konrad Bełzowski. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+}
