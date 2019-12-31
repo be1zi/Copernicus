73d30b10cb80a384d7a32d18386bddbc1e72fd3d
be1zi <konrad.belzowski@gmail.com>
2019-12-31 15:49:04 +0100

    cascade delete

--- Copernicus/Tools/Realm.swift
+++ Copernicus/Tools/Realm.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Konrad Bełzowski. All rights reserved.
 //
 
+import Realm
 import RealmSwift
 import Moya
 
@@ -41,11 +42,78 @@ public extension Realm {
             let realm = try Realm()
             Logger.logInfo(info: realm.configuration.fileURL?.absoluteString ?? "")
             
+            let existedSatellites = realm.objects(SatelliteModel.self)
+            
+            let newKeys = satellites.compactMap { $0.id }
+            
+            let toUpdate = existedSatellites.filter { newKeys.contains($0.id) }
+            
+            let toUpdateKeys = toUpdate.compactMap { $0.id }
+            
+            let toAdd = satellites.filter { !toUpdateKeys.contains($0.id) }
+            let toDelete = existedSatellites.filter { !toUpdateKeys.contains($0.id) }
+
             try realm.write {
-                realm.add(satellites)
+                for object in toUpdate {
+                    if let satellite = existedSatellites.first(where: { $0.id == object.id }) {
+                        satellite.name = object.name
+                        satellite.open = object.open
+                        satellite.geometry = object.geometry
+                    }
+                }
+                
+                realm.add(toAdd)
+                realm.delete(toDelete, cascading: true)
             }
         } catch {
             Logger.logError(error: error)
         }
+    }
+}
+
+//
+// MARK: - Cascading delete
+//
+
+public extension Realm {
+    func delete<S: Sequence>(_ objects: S, cascading: Bool) where S.Iterator.Element: Object {
+        for obj in objects {
+            delete(obj, cascading: cascading)
+        }
+    }
+
+    func delete<Entity: Object>(_ entity: Entity, cascading: Bool) {
+        if cascading {
+            cascadeDelete(entity)
+        } else {
+            delete(entity)
+        }
+    }
+}
+
+private extension Realm {
+    private func cascadeDelete(_ entity: RLMObjectBase) {
+        guard let entity = entity as? Object else { return }
+        var toBeDeleted = Set<RLMObjectBase>()
+        toBeDeleted.insert(entity)
+        while !toBeDeleted.isEmpty {
+            guard let element = toBeDeleted.removeFirst() as? Object,
+                !element.isInvalidated else { continue }
+            resolve(element: element, toBeDeleted: &toBeDeleted)
+        }
+    }
+
+    private func resolve(element: Object, toBeDeleted: inout Set<RLMObjectBase>) {
+        element.objectSchema.properties.forEach {
+            guard let value = element.value(forKey: $0.name) else { return }
+            if let entity = value as? RLMObjectBase {
+                toBeDeleted.insert(entity)
+            } else if let list = value as? RealmSwift.ListBase {
+                for index in 0..<list._rlmArray.count {
+                    toBeDeleted.insert(list._rlmArray.object(at: index) as! RLMObjectBase)
+                }
+            }
+        }
+        delete(element)
     }
 }
