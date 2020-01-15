9588073e685ac442fe34b33da0d5aba1d1631952
Konrad Bełzowski <konrad.belzowski@gmail.com>
2020-01-15 14:48:22 +0100

    cloudy footer view

---
+++ Copernicus/Source/Functionalities/Cloudy/Cells/Footer/CloudyTableViewFooter.swift
@@ -0,0 +1,19 @@
+//
+//  CloudyTableViewFooter.swift
+//  Copernicus
+//
+//  Created by Konrad Belzowski on 15/01/2020.
+//  Copyright © 2020 Konrad Bełzowski. All rights reserved.
+//
+
+import UIKit
+
+public class CloudyTableViewFooter: UITableViewHeaderFooterView {
+    
+    //
+    // MARK: - Properties
+    //
+    
+    @IBOutlet weak var separatorView: UIView!
+    @IBOutlet weak var cloudyLabel: UILabel!
+}
