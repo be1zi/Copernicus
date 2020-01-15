74437285864886e0cf8eeb443606074bdefb4e5f
Konrad Bełzowski <konrad.belzowski@gmail.com>
2020-01-15 14:57:56 +0100

    cloudy header view

---
+++ Copernicus/Source/Functionalities/Cloudy/Cells/Header/CloudyTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  CloudyTableViewHeader.swift
+//  Copernicus
+//
+//  Created by Konrad Belzowski on 15/01/2020.
+//  Copyright © 2020 Konrad Bełzowski. All rights reserved.
+//
+
+import UIKit
+
+public class CloudyTableViewHeader: UITableViewHeaderFooterView {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var satelliteLabel: UILabel!
+    @IBOutlet weak var cloudyLabel: UILabel!
+    @IBOutlet weak var topSeparator: UIView!
+    @IBOutlet weak var leftSeparator: UIView!
+    @IBOutlet weak var rightSeparator: UIView!
+}
