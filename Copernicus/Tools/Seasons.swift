9fab26b9dd516927fd4b45656b0579bb936db87e
Konrad Bełzowski <konrad.belzowski@gmail.com>
2020-01-16 08:16:02 +0100

    calculate seasons

--- Copernicus/Tools/Seasons.swift
+++ Copernicus/Tools/Seasons.swift
@@ -33,33 +33,72 @@ public struct Seasons {
     // MARK: - Properties
     //
     
-    private static func getCurrentSeason() -> Season {
-        let calendar = Calendar.current
-        let currentDate = Date()
+    private static func getCurrentSeason(_ currentDate: Date) -> Season {
         
-        let year = calendar.component(.year, from: currentDate)
+        if Seasons.isInSeason(begin: SeasonDates.springBegin, end: SeasonDates.springEnd, date: currentDate) {
+            return .spring
+        }
         
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd-MM-yyyy"
+        if Seasons.isInSeason(begin: SeasonDates.summerBegin, end: SeasonDates.summerEnd, date: currentDate) {
+            return .summer
+        }
         
-        let springBeginDate = dateFormatter.date(from:"\(SeasonDates.springBegin)-\(String(describing: year))")
-        let springEndDate = dateFormatter.date(from:"\(SeasonDates.springEnd)-\(String(describing: year))")
-        
-        if let sBeginDate = springBeginDate, let sEndDate = springEndDate {
-            if currentDate.compare(sBeginDate) == .orderedDescending && currentDate.compare(sEndDate) == .orderedAscending {
-                return .spring
-            }
+        if Seasons.isInSeason(begin: SeasonDates.autumnBegin, end: SeasonDates.autumnEnd, date: currentDate) {
+            return .autumn
         }
         
         return .winter
     }
     
+    private static func isInSeason(begin: String, end: String, date: Date) -> Bool {
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: date)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        
+        let beginDate = dateFormatter.date(from:"\(begin)-\(String(describing: year))")
+        let endDate = dateFormatter.date(from:"\(end)-\(String(describing: year))")
+        
+        if let firstDayOfSeason = beginDate, let lastDayOfSeason = endDate {
+            if date.compare(firstDayOfSeason) == .orderedDescending && date.compare(lastDayOfSeason) == .orderedAscending {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
     public static func dateYearAgo() -> Date {
         let calendar = Calendar.current
         let currentDate = Date()
+        var year = calendar.component(.year, from: currentDate) - 1
         
-        let year = calendar.component(.year, from: currentDate)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
         
+        let season = getCurrentSeason(currentDate)
+        var dateString = ""
+        
+        switch season {
+        case .spring:
+            dateString = SeasonDates.springBegin
+        case .summer:
+            dateString = SeasonDates.autumnBegin
+        case .autumn:
+            dateString = SeasonDates.winterBegin
+        case .winter:
+            dateString = SeasonDates.springBegin
+            
+            let month = calendar.component(.month, from: currentDate)
+    
+            // if december then get first day of spring in the same year
+            if month == 12 {
+                year += 1
+            }
+        }
+        
+        return dateFormatter.date(from:"\(dateString)-\(String(describing: year))") ?? currentDate
         
     }
 }
